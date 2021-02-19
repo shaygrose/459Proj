49f02e7e1b0a00b3e1d7eab08a40bf4858ac95e8
Shayna <sgrose@sfu.ca>
2021-02-18 20:51:24 -0800

    Adding changes to merging locations

--- src/main.py
+++ src/main.py
@@ -42,6 +42,9 @@ train['age'] = train['age'].apply(
 test['age'] = test['age'].apply(
     lambda x: convert_age_range(x) if isinstance(x, str) else x)
 
+age_means = train.groupby(['outcome'])['age'].mean()
+print(age_means)
+
 # convert date ranges to single date
 train['date_confirmation'] = train['date_confirmation'].apply(
     lambda x: convert_date_range(x) if isinstance(x, str) else x)
@@ -56,6 +59,36 @@ test['date_confirmation'] = pd.to_datetime(
 
 # drop rows that are missing all data that can identify the location, since they are useless
 train.dropna(how='all', subset=['province', 'country', 'longitude', 'latitude'], inplace=True)
+
+
+''' TASK 1.3 '''  # cases_train.csv
+
+# check for impossible values in columns
+outliers = []
+check = [('age', 0, 120), ('latitude', -90, 90), ('longitude', -180, 180)]
+for attr, lower, upper in check:
+    if check_valid(train, attr, lower, upper) !=0:
+        # print("Outlier detected")
+        outliers.append(attr)
+
+assert len(outliers) == 0, "An outlier was detected, perform further investigation!"
+
+train.apply(check_valid_date, axis=1)
+
+# plot = sns.boxplot(x=merged_train['age'])
+# plot = plot.get_figure()
+# plot.savefig("../plots/test.png")
+
+# looking at distribution of ages
+plt.figure(num=1)
+x = train.groupby(['age']).count().reset_index()
+y = np.arange(train['age'].nunique())
+plt.bar(y, x['sex'])
+plt.title("Number of records with different ages")
+plt.xlabel("Age")
+plt.ylabel("Number of records")
+plt.savefig("../plots/age_distribution.png")
+
 
 
 ''' TASK 1.4 '''  # locations.csv
@@ -115,49 +148,34 @@ merged_test.rename(columns={'province_x': 'province', 'country_x': 'country',
 'longitude_x': 'longitude', 'latitude_x': 'latitude'}, inplace=True)
 
 
+# group locations by country and take mean of all values
+# used to assign values to places in cases data that don't have a province match in the locations data
+functions = {'latitude': 'mean', 'longitude': 'mean', 'confirmed': 'mean', 'deaths': 'mean',
+                         'recovered': 'mean', 'active': 'mean', 'combined_key': 'first', 'incidence_rate': 'mean', 'fatality_ratio': 'mean'}
+grouped_locations = locations.groupby(['country']).aggregate(functions)
 
+grouped_locations.drop(columns=['combined_key'])
 
+# print(grouped_locations)
+
+empty = merged_train[pd.isna(merged_train['fatality_ratio'])]
+
+# confirmed,deaths,recovered,active,incidence_rate,fatality_ratio
+
+for index, row in empty.iterrows():
+    #find a country in locations that matches this rows country
+    matching = grouped_locations[row['country']]
+    # if a match exists
+    if matching:
+        row['confirmed'] = matching['confirmed']
+        row['deaths'] = matching['deaths']
+        row['recovered'] = matching['recovered']
+        row['active'] = matching['active']
   
+print(empty)
 
-
-''' TASK 1.3 '''  # cases_train.csv
-
-# check for impossible values in columns
-outliers = []
-check = [('age', 0, 120), ('latitude', -90, 90), ('longitude', -180, 180), ('fatality_ratio', 0, 100)]
-for attr, lower, upper in check:
-    if check_valid(merged_train, attr, lower, upper) !=0:
-        # print("Outlier detected")
-        outliers.append(attr)
-
-assert len(outliers) == 0, "An outlier was detected, perform further investigation!"
-
-
-merged_train.apply(check_valid_date, axis=1)
-
-# plot = sns.boxplot(x=merged_train['age'])
-# plot = plot.get_figure()
-# plot.savefig("../plots/test.png")
-
-# looking at distribution of ages
-plt.figure(num=1)
-x = merged_train.groupby(['age']).count().reset_index()
-y = np.arange(merged_train['age'].nunique())
-plt.bar(y, x['sex'])
-plt.title("Number of records with different ages")
-plt.xlabel("Age")
-plt.ylabel("Number of records")
-plt.savefig("../plots/age_distribution.png")
-
-# print(merged_train['fatality_ratio'].max())
-
-plt.figure(num=2,figsize=(12,6))
-out = pd.cut(merged_train['fatality_ratio'], bins=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20], include_lowest=True)
-ax = out.value_counts(sort=False).plot.bar(rot=0, color="b")
-# ax.set_xticklabels([c[1:-1].replace(","," to") for c in out.cat.categories])
-plt.title("Distribution of fatality ratios")
-plt.xlabel("Fatality ratio")
-plt.ylabel("Number of records")
-plt.savefig("../plots/fatality_ratio_distribution.png")
-
-# merged.apply(detect_outlier, axis=1)
+# aggregate column according to different functions
+aggregation_functions = {'latitude': 'mean', 'longitude': 'mean', 'confirmed': 'sum', 'deaths': 'sum',
+                         'recovered': 'sum', 'active': 'sum', 'combined_key': 'first', 'incidence_rate': 'mean', 'fatality_ratio': 'mean'}
+grouped = us.groupby(['province', 'country']).aggregate(
+    aggregation_functions).reset_index()
