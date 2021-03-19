94ee03589c70746075ae67206eaa86f33bd04e12
Patrick Nguyen <pna18@sfu.ca>
2021-03-18 22:21:29 -0700

    WIP

--- milestone-2/src/main.py
+++ milestone-2/src/main.py
@@ -4,17 +4,14 @@ import pickle
 import sys
 import os
 import csv
-from sklearn.ensemble import AdaBoostClassifier
+import matplotlib.pyplot as plt
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 import xgboost as xgb
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import neighbors
-# Import train_test_split function
+from sklearn import neighbors, metrics
 from sklearn.model_selection import train_test_split
-# Import scikit-learn metrics module for accuracy calculation
-from sklearn import metrics
 import datetime as dt
-import os.path
 from os import path
+from sklearn.metrics import f1_score, classification_report, plot_confusion_matrix, multilabel_confusion_matrix, confusion_matrix
 
 
 data = pd.read_csv("data/cases_train_processed.csv")
@@ -60,10 +57,6 @@ if not path.exists('models/rf_classifier.pkl'):
     rf_model.fit(X_train, y_train)
     pickle.dump(rf_model, open('models/rf_classifier.pkl', 'wb'))
 
-rf_model = pickle.load(open('models/rf_classifier.pkl', 'rb'))
-rf_score = rf_model.score(X_valid, y_valid)
-print("Random Forest score on validation: ", rf_score)
-
 
 ''' ADA BOOST '''
 # ada = AdaBoostClassifier(n_estimators=100, random_state=69)
@@ -81,9 +74,6 @@ if not path.exists('models/xgb_classifier.pkl'):
     # write model to pkl file
     pickle.dump(xg_reg, open('models/xgb_classifier.pkl', 'wb'))
 
-xg_reg = pickle.load(open('models/xgb_classifier.pkl', 'rb'))
-xg_score = xg_reg.score(X_valid, y_valid)
-print("XGBoost score on validation: ", xg_score)
 
 ''' KNN '''
 # normalized = ((data - data.mean())/data.std())
@@ -94,6 +84,106 @@ if not path.exists('models/knn_classifier.pkl'):
     knn.fit(X_train, y_train)
     pickle.dump(knn, open('models/knn_classifier.pkl', 'wb'))
 
+
+''' EVALUATION '''
+
+
+''' Accuracy Score '''
+
+# RF
+rf_model = pickle.load(open('models/rf_classifier.pkl', 'rb'))
+rf_score = rf_model.score(X_valid, y_valid)
+print("Random Forest accuracy score on validation: ", rf_score)
+rf_train_score = rf_model.score(X_train, y_train)
+print("Random Forest accuracy score on train: ", rf_train_score)
+
+# XG
+xg_reg = pickle.load(open('models/xgb_classifier.pkl', 'rb'))
+xg_score = xg_reg.score(X_valid, y_valid)
+print("XGBoost accuracy score on validation: ", xg_score)
+xg_train_score = xg_reg.score(X_train, y_train)
+print("XGBoost accuracy score on train: ", xg_train_score)
+
+# KNN
 knn = pickle.load(open('models/knn_classifier.pkl', 'rb'))
 knn_score = knn.score(X_valid, y_valid)
-print("KNN score on validation: ", knn_score)
+print("KNN accuracy score on validation: ", knn_score)
+knn_train_score = knn.score(X_train, y_train)
+print("KNN accuracy score on train: ", knn_train_score)
+
+
+''' F1 Score '''
+rf_train_pred = rf_model.predict(X_train)
+knn_train_pred = knn.predict(X_train)
+xg_train_pred = xg_reg.predict(X_train)
+rf_valid_pred = rf_model.predict(X_valid)
+knn_valid_pred = knn.predict(X_valid)
+xg_valid_pred = xg_reg.predict(X_valid)
+
+
+# 0:deceased, 1:hospitalized, 2:nonhospitalized, 3:recovered
+target_names = ['0', '1', '2', '3']
+print('--------------------RF Metrics----------------------------------')
+print('- Train -')
+print(classification_report(y_train, rf_train_pred,
+                            target_names=target_names, digits=6))
+print('- Validation -')
+print(classification_report(y_valid, rf_valid_pred,
+                            target_names=target_names, digits=6))
+
+print('-------------------KNN Metrics----------------------------------')
+print('- Train -')
+print(classification_report(y_train, knn_train_pred,
+                            target_names=target_names, digits=6))
+print('- Validation -')
+print(classification_report(y_valid, knn_valid_pred,
+                            target_names=target_names, digits=6))
+
+print('-------------------XGBoost Metrics------------------------------')
+print('- Train -')
+print(classification_report(y_train, xg_train_pred,
+                            target_names=target_names, digits=6))
+print('- Validation -')
+print(classification_report(y_valid, xg_valid_pred,
+                            target_names=target_names, digits=6))
+
+
+# print('--------------------------------------------------------------')
+# print(multilabel_confusion_matrix(y_valid, rf_valid_pred, labels=target_names))
+# print(multilabel_confusion_matrix(y_valid, knn_valid_pred, labels=target_names))
+# print(multilabel_confusion_matrix(y_valid, xg_valid_pred, labels=target_names))
+
+plot_confusion_matrix(knn, X_valid, y_valid,
+                      display_labels=target_names,
+                      cmap=plt.cm.Blues)
+plt.title('KNN Validation Confusion Matrix')
+plt.savefig("plots/knn_valid_confusion.png")
+
+plot_confusion_matrix(knn, X_train, y_train,
+                      display_labels=target_names,
+                      cmap=plt.cm.Blues)
+plt.title('KNN Train Confusion Matrix')
+plt.savefig("plots/knn_train_confusion.png")
+
+plot_confusion_matrix(rf_model, X_valid, y_valid,
+                      display_labels=target_names,
+                      cmap=plt.cm.Blues)
+plt.title('RF Validation Confusion Matrix')
+plt.savefig("plots/rf_valid_confusion.png")
+
+plot_confusion_matrix(rf_model, X_train, y_train,
+                      display_labels=target_names,
+                      cmap=plt.cm.Blues)
+plt.title('RF Train Confusion Matrix')
+plt.savefig("plots/rf_train_confusion.png")
+
+print("XGBoost Train Confusion Matrix")
+print(confusion_matrix(y_train, xg_train_pred))
+print("XGBoost Validation Confusion Matrix")
+print(confusion_matrix(y_valid, xg_valid_pred))
+
+# plot_confusion_matrix(xg_reg, X_valid, y_valid,
+#                       display_labels=target_names,
+#                       cmap=plt.cm.Blues)
+# plt.title('XGBoost Confusion Matrix')
+# plt.savefig("plots/xgb_confusion.png")
