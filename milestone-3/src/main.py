03ad9e23416cd2b4595163add0e20bf143cf48b3
Patrick Nguyen <pna18@sfu.ca>
2021-04-20 22:19:55 -0700

    WIP

--- milestone-3/src/main.py
+++ milestone-3/src/main.py
@@ -11,7 +11,7 @@ from sklearn import neighbors, metrics
 from sklearn.model_selection import train_test_split, GridSearchCV
 import datetime as dt
 from os import path
-from sklearn.metrics import f1_score, classification_report, plot_confusion_matrix, multilabel_confusion_matrix, confusion_matrix
+from sklearn.metrics import f1_score, recall_score, classification_report, plot_confusion_matrix, multilabel_confusion_matrix, confusion_matrix, make_scorer
 
 
 data = pd.read_csv("data/cases_train_processed.csv")
@@ -180,11 +180,53 @@ X_train, X_valid, y_train, y_valid = train_test_split(
 
 ''' TUNING HYPERPARAMS '''
 
+dead_recalls = []
+dead_f1s = []
+overall_recalls = []
+overall_f1s = []
+
+
+def deceased_outcome(val):
+    return True if val == 0 else False
+
+
+def dead_recall_score(model, X, y):
+    y_pred = model.predict(X)
+    # y_pred_dead = np.array(list(map(deceased_outcome, y_pred)))
+    # y_train_dead = np.array(list(map(deceased_outcome, y)))
+
+    # for i in range(len(y_pred)):
+    #     if y_pred_dead[i] and y_train_dead[i]:
+    #         tp += 1
+    #     elif y_train_dead[i] and not y_pred_dead[i]:
+    #         fn += 1
+    # cm = confusion_matrix(y_train_dead, y_pred_dead)
+    # fn = cm[1, 0]
+    # tp = cm[1, 1]
+    # fp = cm[0, 1]
+    # recall = tp / (tp + fn)
+    # precision = tp / (tp + fp) if (tp > 0 or fp > 0) else 0
+    # f1_score = (2 * (precision * recall)) / (precision +
+    #                                          recall) if (precision > 0 or recall > 0) else 0
+    dead_recall = recall_score(y, y_pred, average=None, zero_division=0)[0]
+    dead_f1 = f1_score(y, y_pred, average=None, zero_division=0)[0]
+    dead_recalls.append(dead_recall)
+    dead_f1s.append(dead_f1)
+
+    overall_recalls.append(recall_score(
+        y, y_pred, average='weighted', zero_division=0))
+    overall_f1s.append(recall_score(
+        y, y_pred, average='weighted', zero_division=0))
+    # print("recall", recall)
+    # print("f1 score", f1_score)
+    return dead_recall
+
 # model training
 # create the parameter grid based on the results of random search
 
+
 # RF
-n_estimators = [10, 100, 200]
+n_estimators = [5, 10, 20]
 max_depth = [5, 10, 15]
 min_samples_leaf = [5, 10, 15]
 
@@ -203,7 +245,7 @@ grid_search = GridSearchCV(
     estimator=rf_model,
     param_grid=param_grid,
     # not sure what scoring should be set to ??? need to research more
-    scoring='accuracy',
+    scoring=dead_recall_score,
     verbose=1,
 )
 
@@ -212,5 +254,10 @@ grid_search.fit(X_train, y_train)
 
 best_rf = grid_search.best_estimator_
 best_rf.fit(X_train, X_train)
-print(grid_search.cv_results_)
+print(dead_f1s)
+print(dead_recalls)
+print(overall_f1s)
+print(overall_recalls)
+with open('results/rf_CVresults', 'w') as textfile:
+    print(grid_search.best_params_, file=textfile)
 pickle.dump(best_rf, open('models/best_rf_classifier.pkl', 'wb'))
